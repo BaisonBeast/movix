c60973c1fcf6d8645199cf41d203384579e06391
priyanshusuman <atulatul6666@gmail.com>
2023-09-20 18:39:57 +0530

    useFetch error fixed

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 import Carousel from "../../../components/carousel/Carousel";
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
-import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
-import useFetch from '../../../hooks/UseFetch';
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import UseFetch from "../../../hooks/UseFetch";
 
 function Popular() {
-  const [endpoint, setEndpoint] = useState('movie');
-  const {data, loading} = useFetch(`/${endpoint}/popular`)
-  
+  const [endpoint, setEndpoint] = useState("movie");
+  const { data, loading } = UseFetch(`/${endpoint}/popular`);
+
   const onTabChange = (tab) => {
-    setEndpoint(tab === 'Movies' ? 'movie' : 'tv');
-  }
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
+  };
   return (
-    <div className='carouselSection'>
-        <ContentWrapper className='contentWrapper'>
-            <span className='carouselTitle'>Whats Going Popular</span>
-            <SwitchTabs data={['Movies', 'TV Shows']} onTabChange={onTabChange} />
-        </ContentWrapper>
-        <Carousel data={data?.results} loading={loading} endpoint={endpoint}/> 
+    <div className="carouselSection">
+      <ContentWrapper className="contentWrapper">
+        <span className="carouselTitle">Whats Going Popular</span>
+        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
--- src/pages/home/topRated/TopRated.jsx
+++ src/pages/home/topRated/TopRated.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 import Carousel from "../../../components/carousel/Carousel";
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
-import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
-import useFetch from '../../../hooks/UseFetch';
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import UseFetch from "../../../hooks/UseFetch";
 
 function Popular() {
-  const [endpoint, setEndpoint] = useState('movie');
-  const {data, loading} = useFetch(`/${endpoint}/top_rated`)
-  
+  const [endpoint, setEndpoint] = useState("movie");
+  const { data, loading } = UseFetch(`/${endpoint}/top_rated`);
+
   const onTabChange = (tab) => {
-    setEndpoint(tab === 'Movies' ? 'movie' : 'tv');
-  }
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
+  };
   return (
-    <div className='carouselSection'>
-        <ContentWrapper className='contentWrapper'>
-            <span className='carouselTitle'>Top rated </span>
-            <SwitchTabs data={['Movies', 'TV Shows']} onTabChange={onTabChange} />
-        </ContentWrapper>
-        <Carousel data={data?.results} loading={loading} endpoint={endpoint}/> 
+    <div className="carouselSection">
+      <ContentWrapper className="contentWrapper">
+        <span className="carouselTitle">Top rated </span>
+        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
